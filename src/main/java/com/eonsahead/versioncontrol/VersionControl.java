3e830f611cddd98558d9fc9f7ee4004710c02674
Leon Hannah Tabak <l.tabak@ieee.org>
2021-11-29 10:06:43 -0600

    Begin project.

---
+++ src/main/java/com/eonsahead/versioncontrol/VersionControl.java
@@ -0,0 +1,11 @@
+
+package com.eonsahead.versioncontrol;
+
+
+public class VersionControl {
+
+    public static void main(String[] args) {
+        System.out.println( "Guten Morgen!");
+    } // main( String [] )
+    
+} // VersionControl
